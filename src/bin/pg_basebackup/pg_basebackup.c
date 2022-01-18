9780d5dd7db2e7759ac2b91ec68340c7ad602ca2
Andrey Borodin <amborodin@acm.org>
2022-01-18 10:51:26 +0500

    MDB-15383 enable group access permitions

--- src/bin/pg_basebackup/pg_basebackup.c
+++ src/bin/pg_basebackup/pg_basebackup.c
@@ -512,7 +512,7 @@ verify_dir_is_empty_or_create(char *dirname)
 			/*
 			 * Does not exist, so create
 			 */
-			if (pg_mkdir_p(dirname, S_IRWXU) == -1)
+			if (pg_mkdir_p(dirname, S_IRWXU | S_IRGRP | S_IXGRP) == -1)
 			{
 				fprintf(stderr,
 						_("%s: could not create directory \"%s\": %s\n"),
