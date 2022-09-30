d55101fb3dba14044587264c721d0b6abacd481e
reshke kirill <reshke@double.cloud>
2022-09-30 19:37:05 +0000

    gp parser ext, makefile and stuff
    set gp_parser as trusted extension

---
+++ gpcontrib/gp_parser/annotate_query.c
@@ -0,0 +1,29 @@
+#include "postgres.h"
+#include "fmgr.h"
+#include "utils/builtins.h"
+#include "parser/parser.h"
+#include "nodes/print.h"
+
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+PG_FUNCTION_INFO_V1(annotate_query);
+
+Datum
+annotate_query(PG_FUNCTION_ARGS)
+{
+	text	*sql_t = PG_GETARG_TEXT_P(0);
+	text	*out_t;
+	char	*sql, *out;
+	List	*tree;
+
+	sql = text_to_cstring(sql_t);
+	tree = raw_parser(sql);
+
+	out = nodeToString(tree);
+
+	out_t = cstring_to_text(out);
+	PG_RETURN_TEXT_P(out_t);
+}
