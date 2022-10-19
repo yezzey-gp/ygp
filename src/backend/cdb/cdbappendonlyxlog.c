d4c66dfd6f322e18ea245d3101a126643def9107
reshke kirill <reshke@double.cloud>
2022-10-19 08:18:15 +0000

    Use SMGR in append-optimized storage interactions

--- src/backend/cdb/cdbappendonlyxlog.c
+++ src/backend/cdb/cdbappendonlyxlog.c
@@ -81,6 +81,8 @@ ao_insert_replay(XLogRecord *record)
 	dbPath = GetDatabasePath(xlrec->target.node.dbNode,
 							 xlrec->target.node.spcNode);
 
+	struct f_smgr_ao * smgrao_curr = smgrao();
+
 	if (xlrec->target.segment_filenum == 0)
 		snprintf(path, MAXPGPATH, "%s/%u", dbPath, xlrec->target.node.relNode);
 	else
@@ -92,14 +94,14 @@ ao_insert_replay(XLogRecord *record)
 	/* When writing from the beginning of the file, it might not exist yet. Create it. */
 	if (xlrec->target.offset == 0)
 		fileFlags |= O_CREAT;
-	file = PathNameOpenFile(path, fileFlags, 0600);
+	file = smgrao_curr->smgr_PathNameOpenFile(path, fileFlags, 0600);
 	if (file < 0)
 	{
 		XLogAOSegmentFile(xlrec->target.node, xlrec->target.segment_filenum);
 		return;
 	}
 
-	seek_offset = FileSeek(file, xlrec->target.offset, SEEK_SET);
+	seek_offset = smgrao_curr->smgr_FileSeek(file, xlrec->target.offset, SEEK_SET);
 	if (seek_offset != xlrec->target.offset)
 	{
 		ereport(ERROR,
@@ -110,7 +112,7 @@ ao_insert_replay(XLogRecord *record)
 						path)));
 	}
 
-	written_len = FileWrite(file, buffer, len);
+	written_len = smgrao_curr->smgr_FileWrite(file, buffer, len);
 	if (written_len < 0 || written_len != len)
 	{
 		ereport(ERROR,
