4a32dfa784d2a10ef25baef9ece5556c48dec7ca
reshke kirill <reshke@double.cloud>
2023-04-07 14:47:13 +0000

    Pass skip_cdb_init param to InitPostgres

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -5995,7 +5995,7 @@ BackgroundWorkerInitializeConnection(char *dbname, char *username)
 				(errcode(ERRCODE_PROGRAM_LIMIT_EXCEEDED),
 				 errmsg("database connection requirement not indicated during registration")));
 
-	InitPostgres(dbname, InvalidOid, username, InvalidOid, NULL);
+	InitPostgres(dbname, InvalidOid, username, InvalidOid, NULL, false);
 
 	/* it had better not gotten out of "init" mode yet */
 	if (!IsInitProcessingMode())
@@ -6018,7 +6018,7 @@ BackgroundWorkerInitializeConnectionByOid(Oid dboid, Oid useroid)
 				(errcode(ERRCODE_PROGRAM_LIMIT_EXCEEDED),
 				 errmsg("database connection requirement not indicated during registration")));
 
-	InitPostgres(NULL, dboid, NULL, useroid, NULL);
+	InitPostgres(NULL, dboid, NULL, useroid, NULL, false);
 
 	/* it had better not gotten out of "init" mode yet */
 	if (!IsInitProcessingMode())
