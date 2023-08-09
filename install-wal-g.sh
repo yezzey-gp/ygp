9f80ec734495ee85f3ccf3ec0392f5975efcc68b
Yury Frolov <yura200253@gmail.com>
2023-08-09 10:54:05 +0000

    Simpler WAL-G installation

--- install-wal-g.sh
+++ install-wal-g.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 set -ex
 
-# Install latest Go compiler
-sudo add-apt-repository ppa:longsleep/golang-backports 
-sudo apt update
-sudo apt install -y golang-go
-
-# Install lib dependencies
-sudo apt install -y libbrotli-dev liblzo2-dev libsodium-dev curl cmake
-
-# Fetch project and build
-cd root
-git clone https://github.com/wal-g/wal-g.git
-cd wal-g
-make deps
-make pg_build
+wget -c https://github.com/wal-g/wal-g/releases/download/v2.0.1/wal-g-gp-ubuntu-20.04-amd64.tar.gz
+tar zxvf wal-g-gp-ubuntu-20.04-amd64.tar.gz
+mv wal-g-gp-ubuntu-20.04-amd64 /usr/bin/wal-g
 
 #Check the installation
-main/pg/wal-g --version
+wal-g --version
