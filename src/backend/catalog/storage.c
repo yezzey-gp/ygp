cce5b409bc0bbc7ddf82406eea88bf2107e223bb
reshke kirill <reshke@double.cloud>
2023-09-02 22:14:34 +0000

    Add drop object hook and Bump yezzey

--- src/backend/catalog/storage.c
+++ src/backend/catalog/storage.c
@@ -63,6 +63,8 @@ typedef struct PendingRelDelete
 
 static PendingRelDelete *pendingDeletes = NULL; /* head of linked list */
 
+TrackDropObject_hook_type TrackDropObject_hook = NULL;
+
 /*
  * RelationCreateStorage
  *		Create physical storage for a relation.
@@ -158,6 +160,15 @@ RelationDropStorage(Relation rel)
 	pending->nestLevel = GetCurrentTransactionNestLevel();
 	pending->next = pendingDeletes;
 	pendingDeletes = pending;
+
+
+	/* if yezzey relation, we nned to update relation expire lsn */
+	/*
+	 * Set expiration of the relation's external files.
+	 */
+	if (TrackDropObject_hook) {
+		TrackDropObject_hook(rel);
+	}
 
 	/*
 	 * NOTE: if the relation was created in this transaction, it will now be
