d0b85bf74ad0178a6e162af1ed146b36b51424da
reshke <reshke@double.cloud>
2024-10-28 19:45:44 +0000

    Compeletely remove drop storage hook

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -13500,9 +13500,6 @@ ATExecSetTableSpace(Oid tableOid, Oid newTableSpace, LOCKMODE lockmode)
 	}
 
 	/* drop old relation, and close new one */
-	if (RelationDropStorage_hook) {
-		(*RelationDropStorage_hook)(rel);
-	}
 	RelationDropStorage(rel);
 	smgrclose(dstrel);
 
